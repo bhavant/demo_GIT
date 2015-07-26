7e5e6dfafb7632c23fda83a99b11ec54f764c71d
Bhavan Channira <bhavant@gmail.com>
2015-07-25 20:21:58 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var morgan = require("morgan");
+var parser = require("body-parser");
+
+var app = express();
+
+var port = 3001;
+
+app.get("*", function(req, res) {
+	res.status(200).send("Hello World");
+});
+
+app.listen(port, function(err) {
+	if (err)
+		console.log(err);
+	else
+		console.log("Listening at Port " + port)
+	
+});
